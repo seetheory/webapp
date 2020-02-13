880d9fc9baf84e0c51c5785b9dc6eb47181b5794
seetheory <seetheory@tuta.io>
2020-02-13 08:45:40 +0000

    successful websocket communication

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@
     <div class="title">
       Connecting to websocket
       <br />
-      <input type="text" v-model="command" v-on:keypress="this.textchanged" />
+      <input type="text" v-model="func" v-on:keydown="this.textchanged" />
+      <input type="text" v-model="data" v-on:keydown="this.textchanged" />
       <br />
-      <div style="font-size:20">
-        {{status}}
+      <div class="log">
+        <div class="logline" v-for="line in log">
+          {{line}}
+        </div>
       </div>
     </div>
   </div>
@@ -15,31 +18,53 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import uuid from 'uuid'
 
 @Component({
   name: 'App'
 })
 export default class App extends Vue {
-  status = ''
-  command = ''
+  data = ''
+  func = ''
+  log = [] as string[]
+  lastPayload = {
+    func: '',
+    data: '',
+    _rid: '',
+  }
   ws: WebSocket
   mounted() {
     this.ws = new WebSocket('ws://127.0.0.1:4000')
     this.ws.onclose = () => {
-      this.status = 'closed'
+      this.log.push('disconnected')
     }
     this.ws.onmessage = ({ data }: any) => {
-      this.status = `Received: ${data}`
+      const pretty = JSON.stringify(JSON.parse(data), null, 4)
+      this.log.push(`Received: ${pretty}`)
     }
     this.ws.onopen = () => {
-      this.status = 'connected'
+      this.log.push('connected')
     }
   }
+
   textchanged(e: any) {
     if (e.key === 'Enter') {
       e.preventDefault()
-      if (this.command) this.ws.send(this.command)
-      this.command = ''
+      const payload = {
+        _rid: uuid.v4(),
+        func: this.func,
+        data: JSON.parse(this.data),
+      }
+      if (this.func || this.data) {
+        this.ws.send(JSON.stringify(payload))
+        this.lastPayload = payload
+        this.func = ''
+        this.data = ''
+      }
+    }
+    if (e.key === 'ArrowUp') {
+      this.func = this.lastPayload.func
+      this.data = this.lastPayload.data
     }
   }
 }
@@ -58,6 +83,7 @@ body {
   font-size: 32px;
   font-weight: bold;
   color: white;
+  max-width: 40vw;
 }
 .wrapper {
   display: flex;
@@ -66,5 +92,19 @@ body {
   align-items: center;
   background-color: gray;
   height: 100vh;
+}
+.log {
+  display: flex;
+  max-width: 30vw;
+  flex-direction: column;
+  background-color: black;
+  color: white;
+  font-size: 14px;
+  padding: 4px;
+  padding-top: 0px;
+  font-family: Arial;
+}
+.logline {
+  margin-top: 4px;
 }
 </style>
